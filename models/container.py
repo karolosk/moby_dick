8fccb57ff6a993d71707e3f192ab84d5e045104c
karolosk <karolos.koutsoulelos@gmail.com>
2022-10-03 08:55:55 +0300

    updated model

--- models/container.py
+++ models/container.py
@@ -1,13 +1,13 @@
 class ContainerModel(object):
 
     # Initialize with default values in order to be used as default constructor on the same time
-    def __init__(self, status="", short_id="", name="", cpu_usage="", memory_usage="", memory_max_usage="", host=""):
+    def __init__(self, status="", short_id="", name="", cpu_usage="", memory_usage="", memory_percent="", host=""):
         self.status = status
         self.short_id = short_id
         self.name = name
         self.cpu_usage = cpu_usage
         self.memory_usage = memory_usage
-        self.memory_max_usage = memory_max_usage
+        self.memory_percent = memory_percent
         self.host = host
 
     def to_json(self):
@@ -17,7 +17,7 @@ class ContainerModel(object):
             "name": self.name,
             "cpu_usage": self.cpu_usage,
             "memory_usage": self.memory_usage,
-            "memory_max_usage": self.memory_max_usage,
+            "memory_percent": self.memory_percent,
             "host": self.host
         }
 
