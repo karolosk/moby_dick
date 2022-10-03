8b7c126c75b14e8d1a30df01d9310b66fd4f324b
karolosk <karolos.koutsoulelos@gmail.com>
2022-10-03 08:55:43 +0300

    register web blueprints

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from controllers import container, image
+from controllers.web import container as web_container, index
 
 app = Flask(__name__)
 CORS(app)
@@ -11,5 +12,8 @@ app.register_blueprint(container.api)
 # IMAGE ENDPOINTS
 app.register_blueprint(image.api)
 
+# CONTAINER PAGES
+app.register_blueprint(index.web)
+app.register_blueprint(web_container.web)
 if __name__ == '__main__':
     app.run(debug=True)
